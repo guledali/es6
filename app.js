933a65541545697ab659b5a98247e5200cb3c366
guledali <guled.ali01@gmail.com>
2017-03-22 14:15:14 +0100

    commiting Operator branch

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
 window.onload = function() {
-    /*skapa variabel var har ersatts med let och const*/
-    const pi = 3.142;
+  
+var num1 = [1, 2, 3];
+var num2 = [...num1, 4, 5, 6];
 
-    /*error assisgment to constant variable*/
-   /* pi = 10;*/
-    
-    /*error pi has already been declared*/
-/*    const  pi = 10;
-    console.log(pi);*/
-
-    function calcArea(r) {
-        /*You can redeclare it with const*/
-      /*  const pi = 10;*/
-    console.log("The area is: " + pi * r * r);
-}
-    calcArea(5);
+console.log(num2);
 }
